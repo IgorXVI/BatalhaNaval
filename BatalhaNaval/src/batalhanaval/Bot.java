16725c9a3772b822e5bd40b07588b821657d44c7
Igor Almeida <30632869+IgorXVI@users.noreply.github.com>
2018-07-05 22:30:29 -0300

    Add files via upload

--- BatalhaNaval/src/batalhanaval/Bot.java
+++ BatalhaNaval/src/batalhanaval/Bot.java
@@ -14,18 +14,30 @@ import java.util.Random;
  */
 public class Bot extends Jogador implements Serializable{
     
+    private int xUltimo, yUltimo;
+    private boolean acertouUltimo, navioInimigoVertical;
+    
     public Bot() {
         super("Javaligor");
+        this.xUltimo = -1;
+        this.yUltimo = -1;
+        this.acertouUltimo = false;
     }
 
     public void realizarJogada(Jogador adversario) {
+        boolean bomba = this.gerarBomba();
+        int[] arr = this.gerarPosicoesDeAtaque(adversario, bomba);
+        super.realizarJogada(arr[0], arr[1], adversario, bomba);
+        this.acertouUltimo = (adversario.getTabuleiro().getTabuleiroPublico()[arr[0]][arr[1]] == 'X');
+        this.xUltimo = arr[0];
+        this.yUltimo = arr[1];
+    }
+    
+    private int[] gerarPosicoesDeAtaque(Jogador adversario, boolean bomba){
+        int[] arr = new int[2];
         int x, y;
-        boolean bomba = false;
         char c;
         Random r = new Random();
-        if (this.getTemBomba()) {
-            bomba = r.nextBoolean();
-        }
         x = r.nextInt(7);
         y = r.nextInt(7);
         c = adversario.getTabuleiro().getTabuleiroPublico()[x][y];
@@ -34,6 +46,21 @@ public class Bot extends Jogador implements Serializable{
             y = r.nextInt(7);
             c = adversario.getTabuleiro().getTabuleiroPublico()[x][y];
         }
-        super.realizarJogada(x, y, adversario, bomba);
+        arr[0] = x;
+        arr[1] = y;
+        return arr;
     }
+    
+    private boolean gerarBomba(){
+        boolean bomba;
+        Random r = new Random();
+        if (this.getTemBomba()) {
+            bomba = r.nextBoolean();
+        }
+        else{
+            bomba = false;
+        }
+        return bomba;
+    }
+    
 }
